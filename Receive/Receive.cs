e685ad9e82023af8267d61b2ec6d502426f1c7b1
Galih Satriawan <satriawan.galih99@gmail.com>
2021-01-21 11:56:50 +0700

    first queues

---
+++ Receive/Receive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameClient = "tester";
+            var factory = new ConnectionFactory(){ 
+                UserName = "user",
+                Password = "user",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                HostName = "192.168.1.11"
+                 }; 
+            using (var connection = factory.CreateConnection()){
+                using (var channel = connection.CreateModel()){
+                    channel.QueueDeclare(
+                      queue:  nameClient,
+                      durable: false,
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null
+                    );
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (ModuleHandle, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(
+                        queue: nameClient,
+                        autoAck:true,
+                        consumer: consumer
+                    );
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }     
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create connection
+            var factory = new ConnectionFactory(){ 
+                UserName="user",
+                Password="user",
+                Port= Protocols.DefaultProtocol.DefaultPort, 
+                HostName = "192.168.1.11" };
+            var destination = "tester";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel()){
+                    channel.QueueDeclare(
+                      queue: destination,
+                      durable: false,
+                      exclusive: false,
+                      autoDelete:false,
+                      arguments:null  
+                    );
+                    string message = "Hi, my name is anonymous";
+                    //Publish message
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(
+                      exchange:"",
+                      routingKey:destination,
+                      basicProperties : null,
+                      body: body  
+                    );
+
+                    Console.WriteLine("[x] Sent {0}", message);
+                }
+            }
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
