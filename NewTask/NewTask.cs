9e761823c85cea8a9e667203492d7d0740fa6e58
Galih Satriawan <satriawan.galih99@gmail.com>
2021-01-21 14:40:48 +0700

    add worker lesson

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Connection
+            var factory = new ConnectionFactory{
+                UserName = "user",
+                Password = "user",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                HostName = "192.168.1.11"
+            };
+            var destination = "multiple-workers";
+            using (var connection = factory.CreateConnection()){
+                // Create channel
+                using (var channel = connection.CreateModel()){
+                    channel.QueueDeclare(
+                        queue: destination,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments:null
+                    );
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: destination,
+                        basicProperties: properties,
+                        body: body
+                    );
+                }
+            }
+        }
+        public static string GetMessage(string[] args){
+            return ((args.Length > 0 ) ? string.Join(" ", args): "Hi");
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{
+                UserName = "user",
+                Password = "user",
+                Port = Protocols.DefaultProtocol.DefaultPort,
+                HostName = "192.168.1.11"
+            };
+            var nameClient = "multiple-workers";
+            using (var connection = factory.CreateConnection()){
+                using (var channel = connection.CreateModel()){
+                    // make sure the channel is available
+                    channel.QueueDeclare(
+                        queue: nameClient,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete:false,
+                        arguments:null
+                    );
+
+                    // Consume
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) => {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(" [x] Received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        Console.WriteLine(" [x] Done ");
+
+                    };
+                    channel.BasicConsume(
+                        queue: nameClient,
+                        autoAck: true,
+                        consumer:consumer
+                    );
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
