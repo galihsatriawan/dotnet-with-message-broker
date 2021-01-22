0fe622e5f9b054fcb2825293a1326c32aabeec7d
Galih Satriawan <satriawan.galih99@gmail.com>
2021-01-22 10:29:07 +0700

    Publisher/Subscriber

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{
+                UserName = "user",
+                Password = "user",
+                HostName = "192.168.1.11"
+            };
+            using (var connection = factory.CreateConnection()){
+                using (var channel = connection.CreateModel()){
+                    var exchangeName = "logs";
+                    channel.ExchangeDeclare(exchangeName,ExchangeType.Fanout);
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: exchangeName,
+                        routingKey: "",
+                        basicProperties: null,
+                        body: body
+                    );
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+        public static string GetMessage(string[] args){
+            return ((args.Length > 0 ) ? string.Join(" ", args): "Hi");
+        }
+    }
+}
